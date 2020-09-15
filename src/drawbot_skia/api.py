75b6356ad6b217a7b2198d7e91224b36f59ecac5
justvanrossum <justvanrossum@gmail.com>
2020-09-15 13:27:27 +0200

    formatting/order

--- src/drawbot_skia/api.py
+++ src/drawbot_skia/api.py
@@ -3,16 +3,16 @@ import math
 from .drawbot import Drawbot
 from .path import Path
 
-_db = Drawbot()
 
 __all__ = []
 
+
 _namespace = globals()
+_db = Drawbot()
 
 for _name in dir(math):
     if not _name.startswith("_"):
         __all__.append(_name)
-
 
 for _name in dir(_db):
     if not _name.startswith("_"):
