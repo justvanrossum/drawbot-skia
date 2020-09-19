b8ee15bfa3675835c930840b04088f6f185f9cb2
justvanrossum <justvanrossum@gmail.com>
2020-09-19 21:58:12 +0200

    populate setup.py, use setuptools_scm for versioning

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 from setuptools import setup, find_packages
 
 
+long_description = """\
+drawbot-skia is a cross-plantform reimplementation of the drawing API
+of the [DrawBot application for macOS](https://www.drawbot.com/).
+"""
+
+
 setup(
     name="drawbot-skia",
     use_scm_version={"write_to": "src/drawbot_skia/_version.py"},
+    description="Drawbot implements a simple drawing API to generate 2D vector graphics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Just van Rossum",
+    author_email="justvanrossum@gmail.com",
+    url="http://github.com/justvanrossum/drawbot-skia",
     entry_points={
         'console_scripts': ['drawbot=drawbot_skia.__main__:main'],
     },
@@ -20,5 +32,17 @@ setup(
     setup_requires=["setuptools_scm"],
     python_requires=">=3.6",
     classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Graphics",
+
     ],
 )
