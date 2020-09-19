01cf6781c9326ba4458c5246461dbc2946c6fd79
justvanrossum <justvanrossum@gmail.com>
2020-09-19 20:57:58 +0200

    test multi-script segmented text shaping

---
+++ tests/apitests/text_shaping.py
@@ -0,0 +1,20 @@
+size(600, 500)
+
+fill(1)
+rect(0, 0, width(), height())
+fill(0)
+font("../fonts/IBMPlexSansArabic-Regular.otf")
+fontSize(100)
+openTypeFeatures(ss01=True, ss02=True, ss03=True)
+openTypeFeatures(resetFeatures=True, ss03=True)
+text("Aag0", (50, 50))
+w, h = textSize("Aag0")
+text("---", (50 + w, 50))
+
+openTypeFeatures(resetFeatures=True)
+fontSize(60)
+s = "بلديهما abcde المتحدة"
+text(s, (width() - 50, 150))
+
+text("Alfabet", (width() / 2, 220))
+text("البشريةً", (width() / 2, 290))
