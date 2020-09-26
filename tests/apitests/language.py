24871ca9ed90b7ea9b51f7ea043eb6bf8571d85d
justvanrossum <justvanrossum@gmail.com>
2020-09-26 13:29:37 +0200

    test textSize()

--- tests/apitests/language.py
+++ tests/apitests/language.py
@@ -4,6 +4,10 @@ proof_text = 'вгджзийклнпттттфцчшщъьюѝ'
 
 font('../fonts/SourceSerifPro-Regular.otf')
 fontSize(34)
+w, _ = textSize(proof_text)
+assert round(w, 1) == 498.0
 text(proof_text, (12, 74))
 language("bg")
+w, _ = textSize(proof_text)
+assert round(w, 1) == 537.7
 text(proof_text, (12, 33))
