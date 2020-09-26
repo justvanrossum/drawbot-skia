65de5848774f658c4066ef998e9da51eddd84334
justvanrossum <justvanrossum@gmail.com>
2020-09-26 13:30:54 +0200

    formatting, casing

--- tests/apitests/language.py
+++ tests/apitests/language.py
@@ -1,13 +1,14 @@
 size(600, 120)
 
-proof_text = 'вгджзийклнпттттфцчшщъьюѝ'
-
+proofText = 'вгджзийклнпттттфцчшщъьюѝ'
 font('../fonts/SourceSerifPro-Regular.otf')
 fontSize(34)
-w, _ = textSize(proof_text)
+
+w, _ = textSize(proofText)
 assert round(w, 1) == 498.0
-text(proof_text, (12, 74))
+text(proofText, (12, 74))
+
 language("bg")
-w, _ = textSize(proof_text)
+w, _ = textSize(proofText)
 assert round(w, 1) == 537.7
-text(proof_text, (12, 33))
+text(proofText, (12, 33))
