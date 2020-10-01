a9c5b16fc3d5ce7a836879afc912cb1debc378a0
justvanrossum <justvanrossum@gmail.com>
2020-10-01 19:27:46 +0200

    test for radialGradient()

---
+++ tests/apitests/radialGradient.py
@@ -0,0 +1,8 @@
+size(200, 200)
+
+radialGradient(startPoint=(100, 100), endPoint=(100, 100), colors=[(1, 0, 0), (0, 1, 0), (0, 0, 1)], locations=[0, 0.25, 1], endRadius=100)
+rect(0, 0, 200, 200)
+stroke(0)
+strokeWidth(4)
+fill(1, 0.5)
+rect(50, 50, 100, 100)
