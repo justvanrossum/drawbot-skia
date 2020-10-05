b458e9f9797b8bb3d6c0934291dad8991ef017e8
justvanrossum <justvanrossum@gmail.com>
2020-10-05 10:38:22 +0200

    work in progress: FormattedString class

---
+++ src/drawbot_skia/string.py
@@ -0,0 +1,245 @@
+from bisect import bisect
+from typing import NamedTuple
+from .gstate import FillPaint, StrokePaint, TextStyle, GraphicsStateMixin
+
+
+class FormattedString(GraphicsStateMixin):
+
+    def __init__(self, text=None, runs=None, **properties):
+        self.runs = []
+        self._runCharIndices = None
+        if runs:
+            if text is not None or properties:
+                raise TypeError("can't pass text or properties when initializing from runs")
+            self._appendRuns(runs)
+        else:
+            self.textStyle = TextStyle()
+            self.fillPaint = FillPaint()
+            self.strokePaint = StrokePaint(somethingToDraw=False)
+            self.append(text, **properties)
+
+    def copy(self):
+        copy = FormattedString(runs=self.runs)
+        # Ok to share these, they are copy-on-write, see GraphicsStateMixin
+        copy.textStyle = self.textStyle
+        copy.fillPaint = self.fillPaint
+        copy.strokePaint = self.strokePaint
+        return copy
+
+    def append(self, text, **properties):
+        self._runCharIndices = None
+        if isinstance(text, FormattedString):
+            if properties:
+                raise TypeError("can't pass properties when appending FormattedString")
+            self._appendRuns(list(text.runs))  # Copy, just in case text is self
+            return
+        self._applyProperties(properties)
+        if not text:
+            return
+        self._appendRun(TextRun(text, self.textStyle, self.fillPaint, self.strokePaint))
+
+    def _appendRuns(self, runs):
+        if not runs:
+            return
+        self._appendRun(runs[0])
+        self.runs.extend(runs[1:])
+        lastRun = runs[-1]
+        self.textStyle = lastRun.textStyle
+        self.fillPaint = lastRun.fillPaint
+        self.strokePaint = lastRun.strokePaint
+
+    def _appendRun(self, run):
+        if self.runs:
+            lastRun = self.runs[-1]
+            if (lastRun.textStyle == run.textStyle
+                    and lastRun.fillPaint == run.fillPaint
+                    and lastRun.strokePaint == run.strokePaint):
+                self.runs[-1] = lastRun.addText(run.text)
+                return
+        self.runs.append(run)
+
+    def _applyProperties(self, properties):
+        for prop, value in properties.items():
+            method = getattr(self, prop, None)
+            if method is None:
+                raise TypeError(f"FormattedString does not have a '{prop}' property")
+            if isinstance(value, dict):
+                method(**value)
+            elif isinstance(value, (tuple, list)):
+                method(*value)
+            else:
+                method(value)
+
+    def __eq__(self, other):
+        if not isinstance(other, FormattedString):
+            return NotImplemented
+        return (self.runs == other.runs
+                and self.textStyle == other.textStyle
+                and self.fillPaint == other.fillPaint
+                and self.strokePaint == other.strokePaint)
+
+    def __add__(self, other):
+        result = self.copy()
+        result.append(other)
+        return result
+
+    def __iadd__(self, other):
+        self.append(other)
+        return self
+
+    def __len__(self):
+        return self.runCharIndices[-1]
+
+    def splitlines(self, keepends=False):
+        currentLine = []
+        lines = []
+        for item in self.runs:
+            runParts = item.splitlines(keepends)
+            assert runParts
+            if len(runParts) == 1:
+                currentLine.extend(runParts)
+            else:
+                lines.append(currentLine + runParts[:1])
+                for p in runParts[1:-1]:
+                    lines.append([p])
+                currentLine = runParts[-1:]
+        if currentLine:
+            lines.append(currentLine)
+        return [FormattedString(runs=runs) for runs in lines]
+
+    def join(self, items):
+        result = FormattedString()
+        if items:
+            result.append(items[0])
+            for item in items[1:]:
+                result.append(self)
+                result.append(item)
+        return result
+
+    @property
+    def text(self):
+        return "".join(item.text for item in self.runs)
+
+    @property
+    def runCharIndices(self):
+        if self._runCharIndices is None:
+            pos = 0
+            indices = [pos]
+            for run in self.runs:
+                pos += len(run.text)
+                indices.append(pos)
+            self._runCharIndices = indices
+        return self._runCharIndices
+
+    def findRunIndex(self, characterIndex, previousRunIndex=None):
+        """Given the index of a character into the total string that
+        'self' represents, return the index of the run that contains
+        the character.
+
+        This is needed to retrieve the formatting information needed to
+        render a glyph from a shaped glyph sequence, which may cover
+        multiple runs. The cluster number that HarfBuzz gives us is the
+        character index.
+
+        When iterating over the shaped glyphs, the requested run index
+        for a glyph is likely to be close to the run index returned for
+        the previous glyph: either the it is part of the same run, or of
+        the run before it or after it.
+
+        findRunIndex() can often find the run index faster if it knows
+        the previously returned run index. This is what the
+        `previousRunIndex` argument is for.
+        """
+        numRuns = len(self.runs)
+        if numRuns == 1:
+            # Better: the caller optimizes for this
+            return 0
+        runCharIndices = self.runCharIndices
+        assert 0 <= characterIndex < runCharIndices[-1]
+        assert len(runCharIndices) == numRuns + 1
+        runIndex = previousRunIndex
+        if runIndex is None:
+            # Common case: it's the first or last run
+            if characterIndex < runCharIndices[1]:
+                return 0
+            elif characterIndex >= runCharIndices[-2]:
+                return numRuns - 1
+            # else: fall through, do binary search
+        else:
+            assert 0 <= runIndex < numRuns
+            if runIndex > 0 and characterIndex < runCharIndices[runIndex]:
+                # Maybe it's the previous run
+                runIndex -= 1
+            elif characterIndex >= runCharIndices[runIndex + 1]:
+                # Maybe it's the next run
+                runIndex += 1  # can't overflow because of the first assert
+            if not (runCharIndices[runIndex] <= characterIndex < runCharIndices[runIndex + 1]):
+                # If it isn't the same run, the previous run, or the next run,
+                # give up and do a binary search.
+                runIndex = None
+        if runIndex is None:
+            runIndex = bisect(runCharIndices, characterIndex) - 1
+            assert 0 <= runIndex < numRuns
+        return runIndex
+
+    def iterSplitForShaping(self):
+        #
+        # Font attrs that split shaping:
+        #   font
+        #   fontSize
+        #   lineHeight
+        #   fontVariations
+        #   baselineShift (???)
+        #
+        # Font attrs that won't split shaping:
+        #   align
+        #   tracking
+        #   openTypeFeatures
+        #   baselineShift (???)
+        #
+        # Paint attrs will never split shaping
+        #
+        return self.iterSplit({"font", "fontSize", "lineHeight", "variations"})
+
+    def iterSplit(self, textProperties):
+        prevRun = None
+        currentRuns = []
+        for run in self.runs:
+            if (prevRun is not None and any(
+                    getattr(prevRun.textStyle, prop) != getattr(run.textStyle, prop)
+                    for prop in textProperties)):
+                yield FormattedString(runs=currentRuns)
+                currentRuns = []
+            currentRuns.append(run)
+            prevRun = run
+        if currentRuns:
+            yield FormattedString(runs=currentRuns)
+
+
+class TextRun(NamedTuple):
+
+    text: str
+    textStyle: TextStyle
+    fillPaint: FillPaint
+    strokePaint: StrokePaint
+
+    def addText(self, text):
+        return TextRun(self.text + text, self.textStyle, self.fillPaint, self.strokePaint)
+
+    def __len__(self):
+        return len(self.text)
+
+    def __getitem__(self, indexOrSlice):
+        return TextRun(self.text[indexOrSlice], self.textStyle, self.fillPaint, self.strokePaint)
+
+    def rsplit(self, sep=None, maxsplit=-1):
+        return self._buildSplitParts(self.text.rsplit(sep, maxsplit))
+
+    def split(self, sep=None, maxsplit=-1):
+        return self._buildSplitParts(self.text.split(sep, maxsplit))
+
+    def splitlines(self, keepends=False):
+        return self._buildSplitParts(self.text.splitlines(keepends))
+
+    def _buildSplitParts(self, parts):
+        return [TextRun(t, self.textStyle, self.fillPaint, self.strokePaint) for t in parts]
---
+++ tests/test_string.py
@@ -0,0 +1,177 @@
+import pytest
+from drawbot_skia.string import FormattedString
+
+
+def test_basics():
+    fs = FormattedString()
+    fs.append("Hello")
+    fs.append(" ")
+    fs.append("there.")
+    assert fs.text == "Hello there."
+
+
+def test_good_args():
+    fs = FormattedString(font="A", fontSize=12, fill=(1, 0, 0))
+    fs.append("ABC")
+    fs.fill(0, 1, 0)
+    fs.append("DE")
+    fs.append("F")
+    fs.openTypeFeatures(liga=False)
+    fs.append("fiets")
+    runs = fs.runs
+    assert len(runs) == 3
+    assert runs[0].textStyle.font == "A"
+    assert runs[0].textStyle.fontSize == 12
+    assert runs[0].textStyle.features == {}
+    assert runs[0].fillPaint.color == (255, 255, 0, 0)
+    assert runs[0].text == "ABC"
+    assert runs[1].textStyle.font == "A"
+    assert runs[1].fillPaint.color == (255, 0, 255, 0)
+    assert runs[1].text == "DEF"
+    assert runs[2].textStyle.features == {'liga': False}
+    assert runs[2].text == "fiets"
+
+
+def test_bad_args():
+    with pytest.raises(TypeError):
+        _ = FormattedString(foo=12)
+
+
+iterRunsTestData = [
+    ([("Hallo", {})], ["Hallo"]),
+    ([("Hallo", {}), ("Hallo", {})], ["HalloHallo"]),
+    ([("Hallo", {}), ("Hallo", {"font": "Test"})], ["Hallo", "Hallo"]),
+    ([("Hallo", {}), ("Hallo", {"font": "Test"}), ("Hallo", {})], ["Hallo", "HalloHallo"]),
+]
+
+
+@pytest.mark.parametrize("input, expectedStrings", iterRunsTestData)
+def test_iter_runs(input, expectedStrings):
+    fs = FormattedString()
+    for txt, style in input:
+        fs.append(txt, **style)
+    assert expectedStrings == [r.text for r in fs.runs]
+    assert "".join(expectedStrings) == fs.text
+
+
+def test_iterSplit():
+    fs = FormattedString(font="A", fontSize=10, fill=0)
+    fs.append("abc")
+    fs.fontSize(12)
+    fs.append("def")
+    fs.fill(1, 0, 0)
+    fs.append("ghi")
+    fs.fontSize(10)
+    fs.append("jkl")
+    fs.font("B")
+    fs.append("mno")
+    parts = list(fs.iterSplit(["font", "fontSize"]))
+    texts = [p.text for p in parts]
+    expected_texts = ['abc', 'defghi', 'jkl', 'mno']
+    assert expected_texts == texts
+
+
+@pytest.fixture
+def testString():
+    fs = FormattedString()
+    for i in range(1, 5):
+        fs.fontSize(i + 10)
+        fs.append("a" * i)
+    return fs
+
+
+testFindRunIndicesTestData = [
+    ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 1, 2, 2, 2, 3, 3, 3, 3]),
+    ([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], [3, 3, 3, 3, 2, 2, 2, 1, 1, 0]),
+    ([7, 8, 3, 9, 0, 1, 4, 2, 5, 6], [3, 3, 2, 3, 0, 1, 2, 1, 2, 3]),
+    ([4, 1, 7, 3, 8, 0, 9, 2, 6, 5], [2, 1, 3, 2, 3, 0, 3, 1, 3, 2]),
+]
+
+
+@pytest.mark.parametrize("charIndices, expectedRunIndices", testFindRunIndicesTestData)
+def test_findRunIndex(testString, charIndices, expectedRunIndices):
+    assert len(testString.runs) == 4
+    assert [0, 1, 3, 6, 10] == testString.runCharIndices
+    assert testString.runCharIndices[-1] == len(testString.text)
+    runIndices = []
+    runIndex = None
+    for i in charIndices:
+        runIndex = testString.findRunIndex(i, runIndex)
+        runIndices.append(runIndex)
+    assert expectedRunIndices == runIndices
+
+
+def test_appendFormattedString(testString):
+    numRuns = len(testString.runs)
+    testString.append(testString)
+    assert len(testString.runs) == numRuns * 2
+    testString.append(FormattedString())
+    assert len(testString.runs) == numRuns * 2
+
+
+def test_add(testString):
+    new = testString + "XYZ"
+    assert len(new.runs) == 4
+    assert new != testString
+
+
+def test_iadd(testString):
+    before = testString
+    testString += "XYZ"
+    assert before is testString
+    assert len(testString.runs) == 4
+    assert "aaaaaaaaaaXYZ" == testString.text
+
+
+def test_copy(testString):
+    copy = testString.copy()
+    assert copy is not testString
+    assert copy.runs is not testString.runs
+    assert copy == testString
+    copy.fontSize(123)
+    assert copy != testString
+    copy.fontSize(14)
+    assert copy == testString
+    copy.append("x")
+    assert copy != testString
+    testString.append("x")
+    assert copy == testString
+    assert copy.runs[-1] is not testString.runs[-1]
+    assert copy.runs[-1] == testString.runs[-1]
+    copy.append("y")
+    testString.append("z")
+    assert copy != testString
+    assert copy.runs[-1] != testString.runs[-1]
+
+
+@pytest.mark.parametrize("keepends", [False, True])
+def test_splitlines(testString, keepends):
+    testString.append("\nABC")
+    testString.font("Helvetica")
+    testString.append("DEF\nGHI")
+    lines = testString.splitlines(keepends)
+    assert testString.text.splitlines(keepends) == [line.text for line in lines]
+    joiner = FormattedString("" if keepends else "\n", fontSize=14)
+    joined = joiner.join(lines)
+    assert joined.text == testString.text
+
+
+def test_len(testString):
+    assert len(testString.text) == len(testString)
+    assert 0 == len(FormattedString())
+
+
+def test_textRun_len_slice():
+    testString = FormattedString("abcdefg")
+    run = testString.runs[0]
+    assert 7 == len(run)
+    c = run[0]
+    assert "a" == c.text
+    c = run[:2]
+    assert "ab" == c.text
+    c = run[-1:]
+    assert "g" == c.text
+    c = run[-2:]
+    assert "fg" == c.text
+    c = run[2:5]
+    assert "cde" == c.text
