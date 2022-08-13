87eee4c56aa0a5ba748553aa7f8c6a62d8297086
Just van Rossum <justvanrossum@gmail.com>
2022-08-13 22:14:45 +0200

    Add test scripts and expected results

---
+++ tests/apitests/colrFontVariations.py
@@ -0,0 +1,14 @@
+size(250, 680)
+
+font("../fonts/Nabla.subset.ttf")
+
+fs = 120
+steps = 5
+fontSize(fs)
+for i in range(steps):
+    t = i / (steps - 1)
+    wght = 100 + 600 * t
+    HLGT = t * 10
+    fontVariations(wght=wght, HLGT=HLGT)
+    text("ABC", (20, 20))
+    translate(0, 1.1 * fs)
